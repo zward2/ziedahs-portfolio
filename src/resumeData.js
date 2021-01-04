447e7c5e8bb775c266556f1f54eb91951add9466
Ziedah Ward <ziedahward@Ziedahs-MacBook-Pro.local>
2021-01-04 14:14:57 -0500

    add to resumeData

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,7 +1,7 @@
 let resumeData = {
-    "imagebaseurl":"https://zward2resume.github.io/",
-    "name": "Your name",
-    "role": "Frontend Developer"
+    "imagebaseurl":"https://zward2.github.io/",
+    "name": "Ziedah Ward",
+    "role": "Junior Web Developer"
     }
   export default resumeData
   
