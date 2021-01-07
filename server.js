12ef2e42942b88c68c5528a86af28fca9a1600f2
Ziedah Ward <ziedahward@Ziedahs-MacBook-Pro.local>
2021-01-06 20:02:33 -0500

    fix bug in server

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const app = express();
 
 const port = process.env.PORT || 8080
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 app.use(express.static(__dirname + "/public"));
 
